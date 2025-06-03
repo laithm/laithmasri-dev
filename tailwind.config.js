6b63b0da2aff8908cf32cdccf6fdce185fc93980
laithm <laith.masri@gmail.com>
2025-06-03 20:45:42 +0100

    fixed dark mode toggle? i think

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "monospace"],
+      },
+    },
   },
+  darkMode: "class", // <—— Required for dark toggle to work
   plugins: [],
-}
-
+};
