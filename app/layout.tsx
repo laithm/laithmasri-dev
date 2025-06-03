717c1241ea4aecc7148f6880d1ed6f672da5eb71
laithm <laith.masri@gmail.com>
2025-06-03 19:53:54 +0100

    added black background roots

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-black text-white antialiased ${geistSans.variable} ${geistMono.variable}`}
       >
         {children}
       </body>
